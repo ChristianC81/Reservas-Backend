c748f200ad5dd0d9539ff6c59c71b4a4d07c2b9b
Christian Crespo <110556053+ChristianC81@users.noreply.github.com>
2023-07-23 17:59:03 -0500

    Configuración de EmailController y Application.properties

--- src/main/java/com/tapgroup/pwsalonreservas/controller/EmailController.java
+++ src/main/java/com/tapgroup/pwsalonreservas/controller/EmailController.java
@@ -29,12 +29,12 @@ public class EmailController {
     //? = enviar cualquier tipo de dato
     @PostMapping("/sentCodeVerification")
     public ResponseEntity<?> enviarCorreo(@RequestBody Email e){
-        String mensaje ="Su codigo de Verficiación";
+        String mensaje ="Su codigo de Verificación es:";
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(e.getTo());
         email.setFrom("reservcompany5b@gmail.com");
         email.setSubject(e.getSubject());
-        email.setText(mensaje);
+        email.setText(mensaje+"\n "+e.getText());
         //
         mail.send(email);
 
